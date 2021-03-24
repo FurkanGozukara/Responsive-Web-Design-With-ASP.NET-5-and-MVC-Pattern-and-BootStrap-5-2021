b45ba6f6c2e4d7d20d201c6c37cf8e69f7d92e0c
Furkan Gözükara <furkangozukara@gmail.com>
2021-03-25 00:47:51 +0300

    lecture 4 source code

---
+++ lectures/lecture 4/Classes/testComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using lecture_1.Models;
+
+namespace lecture_1.Classes
+{
+    //[ViewComponent(Name = "TestComponent")]
+    public class testComponent : ViewComponent//it will look views/shared/components/parsedcontroller/default
+    {
+        public async Task<IViewComponentResult> InvokeAsync(int idOfProduct = 100, bool delay = true)
+        {
+            Product myProduct = new Product { Id = idOfProduct, Price = 900, Title = "test product" };
+            //return View("/Views/Shared/Components/testComponent/Parse.cshtml", myProduct);
+            if (delay)
+                System.Threading.Thread.Sleep(5000);
+            return View("Parse", myProduct);
+        }
+    }
+}
---
+++ lectures/lecture 4/GlobalClasses.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+public class GlobalClasses
+{
+    public class customGlobalClass
+    {
+        private int irRank { get; set; }
+
+        public int returnRank()
+        {
+            return irRank;
+        }
+
+        public void setRank(int _irRank)
+        {
+            irRank = _irRank % 5000;
+        }
+    }
+}
+

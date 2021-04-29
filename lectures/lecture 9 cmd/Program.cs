03d698460486a8ebe3b1d39b3a9f047a7cfaf427
Furkan Gözükara <furkangozukara@gmail.com>
2021-04-29 04:52:41 +0300

    lecture 9 source code and DB backup file

---
+++ lectures/lecture 9 cmd/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using lecture_9;
+
+namespace lecture_9_cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            lecture_9.Models.Lecture9 lec9Model = new lecture_9.Models.Lecture9();
+            lec9Model.dtBirthDay = new DateTime(1949, 3, 3);
+            lec9Model.irUserId = 2;
+            lec9Model.srEmail = "da@gmail.com";
+            lec9Model.srEmailRepeated = "a@gmail.com";
+            lec9Model.srFirstName = "ads";
+            lec9Model.srLastName = "12312312312312312312312312312312321";
+
+            var context = new ValidationContext(lec9Model, null, null);
+            var results = new List<ValidationResult>();
+
+            //this first checks regular requirements and if all passes, it then checks IValidatableObject
+            //if any of the regular checks fails, it shortcut and do not check custom IValidatableObject
+            var isValid = Validator.TryValidateObject(lec9Model, context, results, true);
+        
+
+            if (!isValid)
+            {
+                Console.WriteLine("User is NOT valid! Errors:");
+                foreach (var validationResult in results)
+                {
+                    Console.WriteLine(validationResult.ErrorMessage);
+                }
+            }
+            Console.WriteLine("Done...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lectures/lecture 9/Models/Lecture9.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lecture_9.Models
+{
+    public class Lecture9 : IValidatableObject
+    {
+        //all of the possible property attributes data annotations
+        //https://docs.microsoft.com/en-us/dotnet/api/system.componentmodel.dataannotations.displayattribute?view=net-5.0
+        [Display(Name = "First Name:", Description = "Please enter your first name to the text box.", Prompt = "This is the prompt message")]
+        [Required]
+        [StringLength(25, MinimumLength = 3)]
+        public string srFirstName { get; set; }
+
+        [Display(Name = "Last Name:")]
+        [Required(AllowEmptyStrings = true)]
+        public string srLastName { get; set; }
+
+        public string srPostMessage { get; set; }
+
+        [Display(Name = "Email:", Description = "please enter a valid email")]
+        [Required(ErrorMessage = "You have to enter a valid email. Email cant be left empty.")]
+        [EmailAddress]
+        [Compare("srEmailRepeated")]
+        public string srEmail { get; set; }
+
+        public string srEmailRepeated { get; set; }
+
+        [Display(Name = "User Id:", Description = "please enter a valid user id")]
+        [Required]
+        [Range(1, Int32.MaxValue, ErrorMessage = "the entered value was not a valid integer")]
+        public int irUserId { get; set; }
+
+        public DateTime dtBirthDay { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (this.dtBirthDay.Year < 1950)
+                yield return new ValidationResult("Surely you are not THAT old?", new[] { "dtBirthDay" });
+            if (this.dtBirthDay.Year > 2010)
+                yield return new ValidationResult("Sorry, you're too young for this website!", new[] { "dtBirthDay" });
+            if (this.dtBirthDay.Month == 12)
+                yield return new ValidationResult("Sorry, we don't accept anyone born in December!", new[] { "dtBirthDay" });
+            if (!this.srEmail.StartsWith("a"))
+                yield return new ValidationResult("Sorry, your email has to start with letter a", new[] { "srEmail" });
+        }
+    }
+}

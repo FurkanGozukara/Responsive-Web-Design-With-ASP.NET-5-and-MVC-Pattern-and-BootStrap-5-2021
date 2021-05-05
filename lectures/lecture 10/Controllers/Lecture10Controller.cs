7d965461b15bf6ea2e3fcdce45dc48b68d1df4ed
Furkan Gözükara <furkangozukara@gmail.com>
2021-05-06 02:38:13 +0300

    lecture 10 source code

---
+++ lectures/lecture 10/Controllers/Lecture10Controller.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using lecture_9.Models;
+
+namespace lecture_9.Controllers
+{
+    [Route("/")]
+    public class Lecture10Controller : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Lecture10 myModel = new Lecture10 { srTitle = "Welcome to Lecture 10" };
+            if(TempData.ContainsKey("updatedData"))
+                myModel =  TempData.Get<Lecture10>("updatedData");
+               
+
+            return View("Lecture10", myModel);
+        }
+
+        [HttpPost]
+        //[ActionName("updateModelTitle")]
+        public IActionResult updateModelTitle(Lecture10 myModel)
+        {
+            if (!ModelState.IsValid)
+                return Content("error! invalid data is sent to the server");
+
+            myModel.srTitle += ". Data is received.";
+            return View("Lecture10", myModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("/lecture10/update", Name = "updateV2")]
+        public IActionResult updateModelTitleV2(Lecture10 myModel, int irTest = 0)
+        {
+            if (!ModelState.IsValid)
+                return Content("error! invalid data is sent to the server");
+
+            myModel.srTitle += ". Data is received v2.";
+            TempData.Put("updatedData", myModel);
+            return RedirectToAction("Index");
+            //return View("Lecture10", myModel);
+        }
+    }
+}
---
+++ lectures/lecture 10/Models/Lecture10.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lecture_9.Models
+{
+    [Serializable]
+    public class Lecture10
+    {
+        [Display(Name = "New Title:")]
+        public string srTitle { get; set; }
+        public int irCounter { get; set; }
+
+        [Phone]
+        [Display(Name = "Enter your phone number:")]
+        public string PhoneNumber { get; set; }
+
+        [EmailAddress]
+        [Display(Name = "Enter your email:")]
+        public string MailAddress { get; set; }
+
+        [Display(Name = "Set your password:")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Set your birthday:")]
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; } = new DateTime(2000, 1, 1);
+
+        [Display(Name = "Will you be online:")]
+        public bool IsActive { get; set; }
+
+        [Display(Name = "Type your comment as you wish:")]
+        [DataType(DataType.MultilineText)]
+        public string BigLetter { get; set; }
+    }
+}
